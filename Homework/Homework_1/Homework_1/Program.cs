5dbbf63cbfe720320bcbea89a86cd7b8388bda93
mariia tokhtamysh <mariatohtwork@gmail.com>
2024-02-13 12:10:50 +0200

    Убрала все ненужные отступы

--- Homework/Homework_1/Homework_1/Program.cs
+++ Homework/Homework_1/Homework_1/Program.cs
@@ -5,28 +5,21 @@
         static void Main(string[] args)
         {
             int n, j, i, k;
-
             n = 5;
 
             for (k = 1; k <= n; k++)
             {
-
                 for (j = 1; j <= n - k; j++)
                 {
                     Console.Write(" ");
                 }
-
                 for (i = 1; i <= 2 * k - 1; i++)
                 {
                     Console.Write("*");
                 }
                 Console.WriteLine();
             }
-
-
             Console.WriteLine();
-
-
         }
     }
 }
